4768c6fe5a52ec1c44aeb431cbbd976abb294564
Sven Ruppert <sven.ruppert@gmail.com>
2017-06-20 16:34:25 +0200

    init

---
+++ src/test/java/junit/org/rapidpm/vaadin/helloworld/server/junit5/MyUITest.java
@@ -0,0 +1,35 @@
+package junit.org.rapidpm.vaadin.helloworld.server.junit5;
+
+import static junit.framework.TestCase.assertNotNull;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.openqa.selenium.WebElement;
+import junit.org.rapidpm.vaadin.helloworld.server.junit5.selenium.WebDriverSeleniumExtension;
+import junit.org.rapidpm.vaadin.helloworld.server.junit5.vaadin.VaadinTest;
+
+/**
+ *
+ */
+
+@VaadinTest
+public class MyUITest  {
+
+  @ParameterizedTest(name = "{index} ==> ''{0}''")
+  @ArgumentsSource(WebDriverSeleniumExtension.PageObjectProvider.class)
+  void test001(final MyUIPageObject pageObject) {
+    assertNotNull(pageObject);
+
+    pageObject.loadPage();
+
+    pageObject.inputA.get().sendKeys("5");
+    pageObject.inputB.get().sendKeys("5");
+
+    final WebElement btn = pageObject.button.get();
+    btn.click();
+    String value = pageObject.output.get().getAttribute("value");
+    Assertions.assertEquals("10", value);
+  }
+
+}
---
+++ src/test/java/junit/org/rapidpm/vaadin/helloworld/server/junit5/vaadin/HasWebDriver.java
@@ -0,0 +1,11 @@
+package junit.org.rapidpm.vaadin.helloworld.server.junit5.vaadin;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ */
+public interface HasWebDriver {
+  WebDriver getWebDriver();
+  void setWebDriver(WebDriver webDriver);
+}
